35bfb3c45a208209c06778113222be73aedb4d0e
Andy Stanton <andystanton@gmail.com>
2015-03-16 08:59:52 +0000

    start using os/exec

--- dexec.go
+++ dexec.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-  "os"
-	"os/exec"
-  "github.com/codegangsta/cli"
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+    "github.com/codegangsta/cli"
 )
 
 func main() {
-  cli.NewApp().Run(os.Args)
+    app := cli.NewApp()
+    app.Name = "dexec"
+    app.Usage = "dexec"
+    app.Action = func(c *cli.Context) {
+        out, err := exec.Command("docker", "ps").Output()
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("Running Docker processes:\n%s\n", out)
+    }
+
+    app.Run(os.Args)
 }
